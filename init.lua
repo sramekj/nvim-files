78ee1c7c346f622830a3da4e57205dd9ed6069f9
Jan Sramek <jan.sramek@ysoft.com>
2024-05-11 00:36:35 +0200

    fix glsl

--- init.lua
+++ init.lua
@@ -872,5 +872,8 @@ require('lazy').setup({
   },
 })
 
+vim.cmd 'autocmd BufRead,BufNewFile *.frag set filetype=glsl'
+vim.cmd 'autocmd BufRead,BufNewFile *.vert set filetype=glsl'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
